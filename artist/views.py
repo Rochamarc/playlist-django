fa2616bcfb38e8c3bfaecf0bb1045be8d1ab9380
nuke <rochadcmarcos@gmail.com>
2019-10-30 01:01:56 -0300

    views set up

--- artist/views.py
+++ artist/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+# importing my models
+from .models import Band, Album, Song
+
+def index(request):
+    song_list = Song.objects.all()
+    band_list = Band.objects.all()
+    context = { 'song_list': song_list, 'band_list': band_list }
+    # artist/index.html --> arquivo index na pasta de templates no repositorio atual
+    # lembre-se isso não é uma rota
+    return render(request,'artist/index.html', context)
