9f1897a71b083b42c415e3eb8a2a9f0b509f0571
nuke <rochadcmarcos@gmail.com>
2019-11-04 21:27:18 -0300

    band_detail/index views

--- artist/views.py
+++ artist/views.py
@@ -2,16 +2,23 @@ from django.shortcuts import render
 
 # importing my models
 from .models import Band, Album, Song
+# import helpful libs
+from .lib import list_check, artist_check, return_album
 
 def index(request):
     song_list = Song.objects.all()
     band_list = Band.objects.all()
 
-    context = { 'song_list': song_list, 'band_list': band_list }
+    short_song_list = list_check(song_list,5) # return a list with 5 random songs
+
+    artist_rate = artist_check(band_list)
+    band_list = list_check(artist_rate,4) # return a list with 4 random rated bands
+
+    context = { 'song_list': short_song_list, 'band_list': band_list }
     return render(request,'artist/index.html', context) # busca pelo template na pasta template
 
 # Artist index
-def show_bands(request):
+def bands_index(request):
     band_list = Band.objects.all()
     context = { 'band_list': band_list }
     return render(request, 'artist/bands.html', context)
@@ -19,8 +26,8 @@ def show_bands(request):
 # Show details of the artist
 def band_detail(request, pk):
     band_detail = Band.objects.filter(id=pk)
-    song_band_detail = Song.objects.filter(song_artist = band_detail.band_name)
-    album_band_detail = Album.objects.filter(album_artist = band_detail.band_name)
+    song_band_detail = Song.objects.filter(song_artist = band_detail[0])
+    album_band_detail = Album.objects.filter(album_band = band_detail[0])
     context = { 
         'pk': pk, 
         'band_detail': band_detail, 
