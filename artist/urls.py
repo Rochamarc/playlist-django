83ea3a4b18f08782323b0d8cbde2b178f9b6d39d
nuke <rochadcmarcos@gmail.com>
2019-11-07 22:30:43 -0300

    add url album_details

--- artist/urls.py
+++ artist/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('top/', views.top_index, name='top-index'),
     path('bands/',views.bands_index, name='bands-index'),
     path('band/<int:pk>',views.band_detail, name='band-detail'),
+    path('album/<int:pk>',views.album_detail, name='album-detail'),
 ]
